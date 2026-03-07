0a75e1217dd6c9e6d07c60bab4c2b9bddb9fe9c3
Zakhar Voloshchuk <zakhar.voloshcuk@a-team.global>
2026-03-07 13:51:23 +0100

    remove only known extensions

--- app/Routing/Redirectors/FileExtensionRemover.php
+++ app/Routing/Redirectors/FileExtensionRemover.php
@@ -6,8 +6,16 @@ use App\Routing\Redirector;
 
 class FileExtensionRemover implements Redirector
 {
+    /** @var string[] */
+    private array $extensions = ['twig', 'html', 'php'];
+
     public function redirect(string $url): ?string
     {
-        return preg_replace('/\..*$/', '', $url);
+        foreach ($this->extensions as $ext) {
+            if (preg_match("/\\." . preg_quote($ext, '/') . '$/', $url)) {
+                return preg_replace("/\\." . preg_quote($ext, '/') . '$/', '', $url);
+            }
+        }
+        return null;
     }
 }
